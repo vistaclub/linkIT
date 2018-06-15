61a0bc9c737a760dc59c438a6fb1fa162fc01477
Jason Wong <jawong71@gmail.com>
2018-06-15 15:53:36 -0700

    Initial Commit

---
+++ Link It/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Link It
+//
+//  Created by Jason Wong on 2018-06-15.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Link It/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Link It
+//
+//  Created by Jason Wong on 2018-06-15.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
