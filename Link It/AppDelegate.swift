fd23c0a97e2ebf321445e1433ad12141895c5ce3
Jason Wong <jawong71@gmail.com>
2018-06-17 07:15:33 -0700

    Created the status bar and 2 menu items.  One to link to a print function and the other to quit the app.

--- Link It/AppDelegate.swift
+++ Link It/AppDelegate.swift
@@ -11,10 +11,36 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var item : NSStatusItem? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        // Creat the app to be a status bar
+        item = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        // Label the statusbar app as "Link It!"
+        item?.title = "Link It!"
+        /*
+        Code only there as an example for how to add a menu item
+        item?.action = #selector(AppDelegate.linkIt)
+        */
+        
+        // Create the menu items to be shown when clicking the status title
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Link It", action: #selector(AppDelegate.linkIt), keyEquivalent: "L"))
+        menu.addItem(NSMenuItem(title: "Qut", action: #selector(AppDelegate.quit), keyEquivalent: "Q"))
+        
+        // Display menu items
+        item?.menu = menu
+    }
+    
+    func linkIt() {
+        // Print the text when the Link It menu item is pressed
+        print("WE MADE IT!")
+    }
+    
+    func quit() {
+        // Quit the application
+        NSApplication.shared().terminate(self)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
